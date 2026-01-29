29462045ce45e27472e408fbc348feafd141101f
Vimal K <vim9tz@gmail.com>
2026-01-29 11:21:22 +0530

    Initial commit

---
+++ Day_08/circularSinglyLinkedList.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+/* Insert at Beginning */
+struct Node* insertAtBegin(struct Node* head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+
+    if (head == NULL) {
+        newNode->next = newNode;   // points to itself
+        return newNode;
+    }
+
+    struct Node* temp = head;
+    while (temp->next != head)
+        temp = temp->next;
+
+    newNode->next = head;
+    temp->next = newNode;
+
+    return newNode;  // new head
+}
+
+/* Insert at End */
+struct Node* insertAtEnd(struct Node* head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+
+    if (head == NULL) {
+        newNode->next = newNode;
+        return newNode;
+    }
+
+    struct Node* temp = head;
+    while (temp->next != head)
+        temp = temp->next;
+
+    temp->next = newNode;
+    newNode->next = head;
+
+    return head;
+}
+
+/* Insert After a Given Value */
+void insertAfter(struct Node* head, int key, int data) {
+    if (head == NULL)
+        return;
+
+    struct Node* temp = head;
+
+    do {
+        if (temp->data == key) {
+            struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+            newNode->data = data;
+            newNode->next = temp->next;
+            temp->next = newNode;
+            return;
+        }
+        temp = temp->next;
+    } while (temp != head);
+}
+
+/* Display */
+void Display(struct Node* head) {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct Node* temp = head;
+    do {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+
+    printf("(back to head)\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    head = insertAtEnd(head, 5);
+    head = insertAtEnd(head, 10);
+    head = insertAtEnd(head, 20);
+    head = insertAtBegin(head, 1);
+
+    insertAfter(head, 10, 15);
+
+    Display(head);
+
+    return 0;
+}
